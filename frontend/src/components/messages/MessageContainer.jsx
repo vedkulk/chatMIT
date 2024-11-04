fe8ab46707cf29fbe38e58f4e6a444c26f970970
vedkulk <kulkarnived3@gmail.com>
2024-11-04 19:03:15 +0530

    get conversations

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,19 +1,44 @@
+import { useEffect } from "react";
+import useConversation from "../../store/useConversation";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
+import { TiMessages } from "react-icons/ti";
+
 
 const MessageContainer = () => {
+	const { selectedConversation, setSelectedConversation } = useConversation();
+	useEffect(() => {
+		// cleanup function (unmounts)
+		return () => setSelectedConversation(null);
+	}, [setSelectedConversation]);
 	return (
-		<div className='md:min-w-[600px] sm:min-w-[300px] flex flex-col bg-white'>
-			<>
-				{/* Header */}
-				<div className='bg-white-500 border-t border-b  px-4 py-4 mb-2'>
-					<span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John doe</span>
-				</div>
-
-				<Messages />
-				<MessageInput />
-			</>
+		<div className='md:min-w-[600px] flex flex-col bg-white'>
+			{!selectedConversation ? (
+				<NoChatSelected />
+			) : (
+				<>
+					{/* Header */}
+					<div className='border-b border-t border-slate-200 px-4 py-4 mb-2'>
+						<span className='label-text'>To:</span>{" "}
+						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
+					</div>
+					<Messages />
+					<MessageInput />
+				</>
+			)}
 		</div>
 	);
 };
 export default MessageContainer;
+
+const NoChatSelected = () => {
+	return (
+		<div className='flex items-center justify-center w-full h-full bg-white'>
+			<div className='px-4 text-center sm:text-lg md:text-xl text-black font-semibold flex flex-col items-center gap-2'>
+				<p>Welcome to chat<span className="text-red-500">MIT</span>, John Doe </p>
+				<p>Select a chat to start messaging</p>
+				<TiMessages className='text-3xl md:text-6xl text-center' />
+			</div>
+		</div>
+	);
+};
