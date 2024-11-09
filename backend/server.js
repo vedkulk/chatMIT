7b3ad2eb04572b70024773b0ef47b7c42e38e9bc
vedkulk <kulkarnived3@gmail.com>
2024-11-09 17:26:38 +0530

    socketIO backend

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import userRoutes from "./routes/user.routes.js"
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-const app = express();
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB()
     console.log(`Server is running on port ${PORT}`);
 });
