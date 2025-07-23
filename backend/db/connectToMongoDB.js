df5046a6854fb05d21089af881ca90fe3425124a
vedkulk <kulkarnived3@gmail.com>
2025-07-23 10:37:44 +0530

    Fix MongoDB environment variable name mismatch

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const connectToMongoDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_DB_URL)
+        await mongoose.connect(process.env.MONGO_DB_URI)
         console.log("Connected to MongoDB")
     } catch (error) { 
         console.log(error)
